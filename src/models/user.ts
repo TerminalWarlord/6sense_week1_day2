8c1dd0fe949551035654b45a83d68843998773e8
terminalwarlord <joybiswas389@gmail.com>
2026-05-08 11:39:28 +0600

    feat(db): add lastLoggedInAt field in the user schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -33,6 +33,7 @@ const userSchema = new mongoose.Schema({
         enum: ["active", "inactive", "banned"],
         default: "active"
     },
+    lastLoggedInAt: Date
 }, {
     timestamps: true
 })
