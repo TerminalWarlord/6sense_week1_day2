f3dedade6d179273ab707002516fe0e2fd54b5ab
terminalwarlord <joybiswas389@gmail.com>
2026-05-08 08:48:41 +0600

    feat(db): add user schema

---
+++ src/models/user.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    userType: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
+    },
+    status: {
+        type: String,
+        enum: ["active", "inactive", "banned"],
+        default: "active"
+    },
+}, {
+    timestamps: true
+})
+
+export const User = mongoose.model("User", userSchema);
