55b39c0679cfacad9a937395ebcd8b11b1d9e9a7
terminalwarlord <joybiswas389@gmail.com>
2026-05-11 12:03:13 +0600

    feat: add zod validation schema for org management

--- src/validations/organization.validation.ts
+++ src/validations/organization.validation.ts
@@ -3,3 +3,7 @@ import z from "zod";
 export const createOrganizationSchema = z.object({
     name: z.string()
 });
+
+export const manageOrganizationSchema = z.object({
+    organizationId: z.string()
+})
