fe8d467cdfb79a4b37ff631d7dc717533ea8af44
terminalwarlord <joybiswas389@gmail.com>
2026-05-11 14:02:15 +0600

    feat: add route and controller to get user orgs

--- src/routes/organization.route.ts
+++ src/routes/organization.route.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { postCreateOrganization } from "../controllers/organization_controller.ts/create_organization.controller.js";
 import { userMiddleware } from "../middlewares/user.middleware.js";
+import { getOrganizations } from "../controllers/organization_controller.ts/get_organizations.controller.js";
+import { adminMiddleware } from "../middlewares/admin.middleware.js";
 
 export const organizationRouter = Router();
 
 organizationRouter.post("/create", userMiddleware, postCreateOrganization);
+// TODO: Need to work on a better middleware
+organizationRouter.get("/get", adminMiddleware, getOrganizations);
