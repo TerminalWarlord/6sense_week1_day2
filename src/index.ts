717a3f3a9f0daf8d79ee03b1d1d1b172e5dfba9b
terminalwarlord <joybiswas389@gmail.com>
2026-05-11 12:43:18 +0600

    chore: add debug log

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { type NextFunction, type Request, type Response } from "express
 import "dotenv/config";
 import { connectDB } from "./lib/db.js";
 import { userRouter } from "./routes/user.route.js";
+import { organizationRouter } from "./routes/organization.route.js";
 
 const PORT = Number(process.env.PORT) || 3000;
 const app = express();
@@ -9,12 +10,14 @@ const app = express();
 app.use(express.json());
 
 app.use('/user', userRouter);
+app.use('/organization', organizationRouter);
 app.get("/health", (req, res) => {
     res.json({
         message: "Successful"
     })
 });
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(err);
     res.status(500).json({
         message: "Internal server error"
     })
